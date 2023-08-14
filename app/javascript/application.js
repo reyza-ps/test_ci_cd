29d43a4bfcf2a177902b8d4f593c73f6eb2a7264
Yunan Helmy <m.yunan.helmy@gmail.com>
2023-08-15 06:30:20 +0700

    Admin layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,32 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// headerbar
+const userMenuButton = document.getElementById('user-menu-button')
+const userMenu = document.getElementById('user-menu')
+
+userMenuButton.addEventListener('click', () => {
+  if (userMenu.classList.contains('hidden')){
+    userMenu.classList.remove("hidden")
+    userMenu.classList.add("block")
+  } else {
+    userMenu.classList.remove("block")
+    userMenu.classList.add("hidden")
+  }
+})
+
+// Menu mobile
+const closeSidebarButton = document.getElementById('close-sidebar')
+const openSidebarButton = document.getElementById('open-sidebar')
+const menuMobile = document.getElementById('menu-mobile')
+
+openSidebarButton.addEventListener('click', () => {
+  menuMobile.classList.remove("hidden")
+  menuMobile.classList.add("block")
+})
+
+closeSidebarButton.addEventListener('click', () => {
+  menuMobile.classList.remove("block")
+  menuMobile.classList.add("hidden")
+})
